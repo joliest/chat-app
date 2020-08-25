a4103b6b8a1312f706fcf02a52f2054003770014
Joliver Estampador <joliverestampador@gmail.com>
2020-08-25 20:47:55 +0800

    Setup nodemon

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const path = require('path')
 const app = express();
 
+const PORT = process.env.PORT || 3000
+
 const PUBLIC_FOLDER = path.join(__dirname, '../public')
 app.use(express.static(PUBLIC_FOLDER))
 
-app.listen(3000, () => {
-    console.log('listening to 3000')
+app.listen(PORT, () => {
+    console.log(`listening to ${PORT}`)
 })
