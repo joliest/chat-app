e060635de2bc19306e70348909f068be47630a16
Joliver Estampador <joliverestampador@gmail.com>
2020-08-27 20:42:45 +0800

    Initial commit for a web socket

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const path = require('path')
-const app = express();
+const http = require('http')
+const socketio = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 const PORT = process.env.PORT || 3000
 
 const PUBLIC_FOLDER = path.join(__dirname, '../public')
 app.use(express.static(PUBLIC_FOLDER))
 
-app.listen(PORT, () => {
+io.on('connection', () => {
+    console.log('New Websocket Connection')
+})
+
+server.listen(PORT, () => {
     console.log(`listening to ${PORT}`)
 })
