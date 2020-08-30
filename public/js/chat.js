2aafda6a643dc3cf42586f0591ec4e367a5c661b
Joliver Estampador <joliverestampador@gmail.com>
2020-08-30 13:25:40 +0800

    Sending your location

--- public/js/chat.js
+++ public/js/chat.js
@@ -12,3 +12,14 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
     const message = e.target.elements.message.value
     socket.emit('sendMessage', message)
 });
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geo-location is not supported by your browser.')
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        const {latitude, longitude} = position.coords;
+        socket.emit('sendLocation', {latitude, longitude})
+    })
+})
