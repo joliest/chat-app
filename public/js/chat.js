19d2a1640d94f305626e06877f0f4f7af5a36d72
Joliver Estampador <joliverestampador@gmail.com>
2020-09-06 13:34:20 +0800

    Basic mustache library

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,9 +4,18 @@ const $messageForm = document.querySelector('#message-form')
 const $messageFormInput = $messageForm.querySelector('input')
 const $messageFormButton = $messageForm.querySelector('button')
 const $sendLocationButton = document.querySelector('#send-location');
+const $messages = document.querySelector('#messages')
+
+// templates
+const messageTemplate = document.querySelector('#message-template')
+                                .innerHTML
 
 socket.on('message', message => {
-    console.log(message)
+    const html = Mustache.render(messageTemplate, {
+        // can be accessed in {{ }}
+        message
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
 })
 
 const sendButton = document.querySelector('#send-button')
