4c685bd0db38e97d7aa56004890ae0633441c6cd
Joliver Estampador <joliverestampador@gmail.com>
2020-09-10 20:45:25 +0800

    Styling chat app

--- public/js/chat.js
+++ public/js/chat.js
@@ -8,7 +8,7 @@ const $messages = document.querySelector('#messages')
 
 // templates
 const messageTemplate = document.querySelector('#message-template').innerHTML
-const locationTemplate = document.querySelector('#location-template').innerHTML
+const locationTemplate = document.querySelector('#location-message-template').innerHTML
 
 socket.on('message', message => {
     const html = Mustache.render(messageTemplate, {
