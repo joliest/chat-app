84f83f62f2c8975d73d0744bfd2056cd4cbbfecb
Joliver Estampador <joliverestampador@gmail.com>
2020-09-19 11:12:26 +0800

    Storing users methods

---
+++ src/utils/users.js
@@ -0,0 +1,58 @@
+const users = []
+
+const addUser = ({ id, username, room }) => {
+    // Clean the data
+    username = username.trim().toLowerCase()
+    room = room.trim().toLowerCase()
+
+    // Validate the data
+    if (!username || !room) {
+        return {
+            error: 'Username and room are required!'
+        }
+    }
+
+    // Check for existing user
+    const existingUser = users.find((user) => {
+        return user.room  === room && user.username === username
+    })
+
+    // validate user
+    if (existingUser) {
+        return {
+            error: 'Username is in use!'
+        }
+    }
+
+    // store user
+    const user = { id, username, room }
+    users.push(user)
+    return { user }
+}
+
+
+const removeUser = (id) => {
+    const index = users.findIndex(user => user.id === id)
+
+    if (index !== -1) {
+        // second arg - number of items you want to remove
+        return users.splice(index, 1)[0]
+    }
+}
+
+const getUser = (id) => {
+    return users.
+    find(user => user.id === id)
+}
+
+const getUserInRoom = (room) => {
+    room = room.trim().toLowerCase()
+    return users.filter(user => user.room === room)
+}
+
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUserInRoom
+}
